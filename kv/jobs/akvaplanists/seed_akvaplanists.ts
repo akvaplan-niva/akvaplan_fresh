bd936ec650ee6b7dfda84e8afd33ab7a8d0ab831
cnrdh <cnrdh@users.noreply.github.com>
2024-01-23 15:53:54 +0100

    added

---
+++ kv/jobs/akvaplanists/seed_akvaplanists.ts
@@ -0,0 +1,27 @@
+import { openKv } from "akvaplan_fresh/kv/mod.ts";
+import { getAkvaplanists } from "akvaplan_fresh/services/akvaplanist.ts";
+import type { SlimPublication } from "akvaplan_fresh/@interfaces/slim_publication.ts";
+
+export const getExistingIds = async () => {
+  const existing = new Set<string>();
+  const kv = await openKv();
+  for await (const { value } of kv.list({ prefix: ["akvaplanists"] })) {
+    existing.add(value.id);
+  }
+  return existing;
+};
+
+export const seedAkvaplanists = async () => {
+  const kv = await openKv();
+  const existing = await getExistingIds();
+  const apns = await getAkvaplanists();
+
+  apns.map(async (person) => {
+    const { id } = person;
+    const key = ["akvaplanists", id];
+    if (!existing.has(id)) {
+      person.created = new Date().toJSON();
+    }
+    await kv.set(key, person);
+  });
+};
