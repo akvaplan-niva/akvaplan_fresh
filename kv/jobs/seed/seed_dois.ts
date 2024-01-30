3cd96cf306272f69b6d8c15f0e6dd1f61ae86c3a
cnrdh <cnrdh@users.noreply.github.com>
2024-01-30 15:55:36 +0100

    new files

---
+++ kv/jobs/seed/seed_dois.ts
@@ -0,0 +1,13 @@
+import { openKv } from "akvaplan_fresh/kv/mod.ts";
+import { getDois } from "akvaplan_fresh/services/dois.ts";
+import type { SlimPublication } from "akvaplan_fresh/@interfaces/slim_publication.ts";
+
+export const seedDois = async () => {
+  const kv = await openKv();
+  const { data } = await getDois();
+  data.map(async (value: SlimPublication) => {
+    const [prefix, suffix] = value.doi.split("/");
+    const key = ["dois", prefix, suffix];
+    await kv.set(key, value);
+  });
+};
