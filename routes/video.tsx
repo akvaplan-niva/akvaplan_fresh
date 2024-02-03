402476ce0dc0a2ba71276b646f3627aee1c5dad5
cnrdh <cnrdh@users.noreply.github.com>
2024-02-03 17:45:25 +0100

    fix video

--- routes/video.tsx
+++ routes/video.tsx
@@ -1,10 +1,11 @@
-import { getVideo } from "akvaplan_fresh/kv/video.ts";
+import { getV, getVideo } from "akvaplan_fresh/kv/video.ts";
 import { extractId } from "../services/extract_id.ts";
 
 import { Page } from "akvaplan_fresh/components/mod.ts";
 import { VideoArticle } from "../components/VideoArticle.tsx";
 
 import type { RouteConfig, RouteContext } from "$fresh/src/server/types.ts";
+import { getValue, openKv } from "akvaplan_fresh/kv/mod.ts";
 
 export const config: RouteConfig = {
   routeOverride: "/:lang(no|en)/:type(film|video){/:date}?/:slug",
@@ -13,13 +14,13 @@ export const config: RouteConfig = {
 export default async function VideoPage(req: Request, ctx: RouteContext) {
   const { slug } = ctx.params;
   const id = extractId(slug);
-  const { value, versionstamp } = await getVideo(Number(id));
-  if (!versionstamp) {
+  const video = await getVideo(Number(id));
+  if (!video) {
     return ctx.renderNotFound();
   }
   return (
-    <Page title={value.header}>
-      <VideoArticle item={value} embed={value.embed} />
+    <Page title={video.header}>
+      <VideoArticle item={video} embed={video.embed} />
     </Page>
   );
 }
