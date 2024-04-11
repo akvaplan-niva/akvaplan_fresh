84748ddedf68e6ae0c1e57b10df8ad52dcac396c
cnrdh <cnrdh@users.noreply.github.com>
2024-04-11 09:49:55 +0200

    add starttime=0

--- components/VideoArticle.tsx
+++ components/VideoArticle.tsx
@@ -9,9 +9,9 @@ export function VideoArticle(
       <div style="position:relative;width:100%;padding-bottom:56.25%;">
         <iframe
           allow="autoplay; fullscreen"
-          allowFullScreen
+          allowfullscreen
           referrerpolicy="no-referrer-when-downgrade"
-          src={`https://api.screen9.com/embed/${item.embed}`}
+          src={`https://api.screen9.com/embed/${item.embed}?starttime=0`}
           style="border:0;width:100%;height:100%;position:absolute"
           title=""
         >
