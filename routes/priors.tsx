ccba969c47ec20069d28d919792a46da39b2a4fb
cnrdh <cnrdh@users.noreply.github.com>
2024-10-10 14:07:50 +0200

    simplify

--- routes/priors.tsx
+++ routes/priors.tsx
@@ -8,23 +8,23 @@ import type { Akvaplanist } from "akvaplan_fresh/@interfaces/akvaplanist.ts";
 
 import { asset, Head } from "$fresh/runtime.ts";
 import type { RouteConfig, RouteContext } from "$fresh/server.ts";
+import { personURL } from "akvaplan_fresh/services/mod.ts";
 
 export const config: RouteConfig = {
-  routeOverride: "/:lang(no|en)/akvaplanist{/:which(prior)}?",
+  routeOverride: "/:lang(no|en)/akvaplanist{/:which(prior|expired)}?",
 };
 
 const getAkvaplanistsInExternalKvService = async (kind: string) => {
   const r = await fetch(`https://akvaplanists.deno.dev/kv/${kind}`);
-  const sortkey = kind === "expired" ? "expired" : "from";
+  const sortkey = "name"; //["expired", "prior"].includes(kind) ? "expired" : "from";
   const arr: Akvaplanist[] = (await Array.fromAsync(await r.json())).map((
     { value }: { value: Akvaplanist },
   ) => value)
     .map((a) => {
+      a.name = [a.given, a.family].join(" ");
       a.from = a.from ?? a.created;
       return a;
-    }).sort((a, b) =>
-      new Date(b[sortkey]).getTime() - new Date(a[sortkey]).getTime()
-    );
+    }).sort((a, b) => a.name?.localeCompare(b.name));
   return arr;
 };
 
@@ -33,9 +33,10 @@ export default async function PriorsPage(req: Request, ctx: RouteContext) {
   const kind = ["expired", "prior"].includes(which) ? "expired" : "person";
 
   const priors = (await getAkvaplanistsInExternalKvService(kind)).map((p) => {
-    const { id, family, given } = p;
+    const { id, family, given, expired } = p;
     const slug = `id/${id}/${given}+${family}`;
-    p.homepage = href({ slug, collection: "person", lang }) as string;
+    p.href = personURL(p, lang);
+    p.name = [given, family].join(" ");
     return p;
   });
 
@@ -44,6 +45,13 @@ export default async function PriorsPage(req: Request, ctx: RouteContext) {
 
   return (
     <Page title={which} collection={collection}>
+      <ol>
+        {priors.map((p) => (
+          <li>
+            <a href={p.href}>{p.name}</a>
+          </li>
+        ))}
+      </ol>
       <section class="Section">
         <main
           style={{
