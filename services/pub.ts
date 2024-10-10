c0df55197fb358fc5c83288556e21db2f416b7fb
cnrdh <cnrdh@users.noreply.github.com>
2024-10-10 13:29:24 +0200

    remove log; add helper

--- services/pub.ts
+++ services/pub.ts
@@ -1,12 +1,11 @@
 import { SlimPublication } from "akvaplan_fresh/@interfaces/mod.ts";
-
 export const PUBS_BASE = globalThis.Deno && Deno.env.has("AKVAPLAN_PUBS")
   ? Deno.env.get("AKVAPLAN_PUBS")
   : "https://pubs.deno.dev";
 
 export const fetchPubFromAkvaplanService = async (uri: string) => {
   const url = new URL(`/pub/${uri}`, PUBS_BASE);
-  console.warn(url.href);
+  //console.warn(url.href);
   return await fetch(url);
 };
 
@@ -22,4 +21,11 @@ export const fetchNvaMetadataFromAkvaplanService = async (id: string) => {
   const url = new URL(`/nva/${id}`, PUBS_BASE);
   console.warn(url.href);
   return await fetch(url);
+};
+
+export const getNvaMetadata = async (identifier: string) => {
+  const r = await fetchNvaMetadataFromAkvaplanService(identifier);
+  if (r?.ok) {
+    return await r.json();
+  }
 };
