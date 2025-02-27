5cfd317b8d0ba7640ea029a39e524d792cff8000
cnrdh <cnrdh@users.noreply.github.com>
2025-02-27 11:04:08 +0100

    fix pub link

--- routes/pub_multiplicates.tsx
+++ routes/pub_multiplicates.tsx
@@ -3,15 +3,20 @@ import { Card, Page } from "akvaplan_fresh/components/mod.ts";
 import { defineRoute, type RouteConfig } from "$fresh/server.ts";
 import { pubsURL } from "akvaplan_fresh/services/nav.ts";
 import { SearchResultItem } from "akvaplan_fresh/components/search_result_item.tsx";
+import { atomizeSlimPublication } from "akvaplan_fresh/search/indexers/pubs.ts";
 
 export const config: RouteConfig = {
   routeOverride: "/:lang(en|no)/:page(pubs|publications|publikasjoner)/multi",
 };
 
 const getMultiplicates = async () => {
-  const all = (await Array.fromAsync(await pubs())).map(({ value }) => value);
+  const all = await Array.fromAsync(await pubs());
+  const atoms = await Array.fromAsync(all.map(async (
+    { value },
+  ) => await atomizeSlimPublication(await value)));
+
   const grouped = Map
-    .groupBy(all, (pub) => pub.title);
+    .groupBy(atoms, (pub) => pub.title);
 
   return [...grouped]
     .filter(([title, list]) => list.length > 1)
@@ -21,6 +26,7 @@ const getMultiplicates = async () => {
 export default defineRoute(async (req, ctx) => {
   const { lang } = ctx.params;
   const multi = await getMultiplicates();
+
   return (
     <Page
       title={""}
