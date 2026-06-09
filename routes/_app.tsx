3f9d2799f25d0d02e33adadc486fd2aa875b34ff
cnrdh <cnrdh@users.noreply.github.com>
2026-06-09 09:21:16 +0200

    geist

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,22 +7,8 @@ export default function App(props: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title></title>
-
+        <title>Akvaplan-niva</title>
         <meta property="og:site_name" content="Akvaplan-niva" />
-        {
-          /* <script
-          type="application/json"
-          id="state"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(state) }}
-        /> */
-        }
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Geist+Mono:ital,wght@0,100..900;1,100..900&family=Geist:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
         <link href="/styles.css" rel="stylesheet" />
       </head>
       <body>
