07a7cc37caacd675a2406a60002b0305b84b7335
cnrdh <cnrdh@users.noreply.github.com>
2026-07-06 13:07:15 +0200

    handle slash

--- components/site_lang_links.tsx
+++ components/site_lang_links.tsx
@@ -1,29 +1,36 @@
-import { getSiteLang, lang as langSignal, languages, t } from "@/text/mod.ts";
+import { lang as langSignal, languages, t } from "@/text/mod.ts";
 
-const getCanonicalUrl = (url: string | URL, lang: string, hreflang: string) => {
-  const u = new URL(url ?? "https://akvaplan.no");
+const defaultUrl = "https://akvaplan.no";
+
+const getCanonicalHref = (
+  url: URL,
+  lang: string,
+  hreflang: string,
+) => {
+  const u = new URL(url ?? defaultUrl);
   if (languages.has(u.pathname.slice(1, 3))) {
     return u.pathname && lang && hreflang
       ? u.pathname?.replace(lang, hreflang) + "" + u.search
-      : hreflang
-      ? `/${hreflang}`
-      : getSiteLang();
+      : `/${hreflang}`;
   } else if (["@", "~"].includes(u.pathname?.substring(1, 2))) {
     const c1 = u.pathname[1] === "@" ? "~" : "@";
     u.pathname = "/" + c1 + u.pathname.slice(2);
+    return u.pathname;
+  } else if ("/" === u.pathname) {
+    return u.pathname;
   } else {
-    console.warn("Unknown canonical URL", url);
-    return url;
+    console.warn("Unknown canonical URL", url.href);
+    return u.pathname;
   }
 };
 export const SiteLangLinks = (
-  { lang = langSignal.value, url, ...props } = {},
+  { lang = langSignal.value, url = defaultUrl, ...props } = {},
 ) => {
   const native = t(`lang.Native.${lang}`);
   const u = url ? new URL(url) : null;
   const hreflang = lang === "en" ? "no" : "en";
-  const href = u
-    ? getCanonicalUrl(u, lang, hreflang)
+  const href = u && "pathname" in u
+    ? getCanonicalHref(u, lang, hreflang)
     : lang === "en"
     ? "/no"
     : "/en";
